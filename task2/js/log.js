1b326d0b578d454657f6981d1a5ec27025c033e9
valery.vorontsov <valery.vorontsov@solbeg.com>
2019-03-05 16:48:23 +0300

    simple else

--- task2/js/log.js
+++ task2/js/log.js
@@ -5,14 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
         var type = obj.getType();
         var typeValue = obj.getTypeValue();
 
-        sumObj[type] = sumObj[type] + typeValue || typeValue;
+        sumObj[type] = (sumObj[type] || 0) + typeValue;
     });
 
     for (var prop in sumObj) {
         console.log(`Sum of type ${prop} = ${sumObj[prop]}`);
 
         var newTotal = document.createElement('div');
-
         newTotal.textContent = `Sum of type ${prop} = ${sumObj[prop]}`;
 
         document.querySelector('.totals').appendChild(newTotal);
